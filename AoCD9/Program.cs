646f8e4635ea047c4c05c4be683ef1bf4645d80a
Diego <98835176+diegos79@users.noreply.github.com>
2022-07-23 19:14:34 +0200

    Add files via upload

--- AoCD9/Program.cs
+++ AoCD9/Program.cs
@@ -5,59 +5,73 @@ using System.IO;
 namespace AOCD9
 {
 
-    public class LowPoints
+    public class Points
     {
-        public int LowPoint { get; set; }
+        public int PointValue { get; set; }
         public int CoordX { get; set; }
         public int CoordY { get; set; }
         public int BasinValue { get; set; }
-        public LowPoints (int lowPoint, int coordX, int coordY)
+
+        public int CountBasin { get; set; }
+
+        public Points(int pointValue, int coordX, int coordY)
         {
-            LowPoint = lowPoint;
+            PointValue = pointValue;
             CoordX = coordX;
             CoordY = coordY;
         }
 
-        public LowPoints(int lowPoint, int coordX, int coordY, int basinValue)
+        public Points(int pointValue, int countBasin)
         {
-            LowPoint = lowPoint;
+            PointValue = pointValue;
+            CountBasin = countBasin;
+        }
+        public Points(int pointValue, int coordX, int coordY, int basinValue)
+        {
+            PointValue = pointValue;
             CoordX = coordX;
             CoordY = coordY;
             BasinValue = basinValue;
         }
 
-
-        public LowPoints() { }
     }
 
     public class Program
     {
         public static void Main()
         {
+
             var stringArray = ConvertInputToStringArray("sample.txt");
             var matrix = SetMatrix(stringArray);
             PrintMatrix(matrix);
             var lowPoints = GetLowPoints(matrix);
             var risk = GetRiskLevel(lowPoints);
             Console.WriteLine($"The risk level is: {risk}");
-            var getLowPointsCoords = GetLowPointsCoords(matrix);
-            foreach (var item in getLowPointsCoords)
+
+            foreach (var item in lowPoints) 
             {
-                Console.Write("Coordinates of the Low Points: ");
+                Console.Write($"Coordinates of the Low Points {item.PointValue}: ");
                 Console.WriteLine(item.CoordX.ToString()+" "+ item.CoordY.ToString());
             }
-            var y2 = GetLowPointsBasin(matrix, getLowPointsCoords);
+            var basinPoints = GetBasinPoints(matrix, lowPoints);
+            var countBasin = CountBasin(matrix, lowPoints);
             Console.WriteLine();
-            foreach (var item in y2)
+            foreach (var item in basinPoints)
             {
-                Console.WriteLine($"Low Point Basin on lowpoint: {item.LowPoint} X: {item.CoordX} Y: {item.CoordY} Basin: {item.BasinValue}");
-
+                Console.WriteLine($"Low Point Basin on lowpoint: {item.PointValue} X: {item.CoordX} Y: {item.CoordY} Basin: {item.BasinValue}");
             }
-            
-            Console.WriteLine($"Total low basin points: {y2.Count}");
+
+            foreach (var item in countBasin)
+            {
+                Console.WriteLine($"LowPoint {item.Key.PointValue} has a basin of ---> {item.Value} elements");
+            }
+
+
+            Console.WriteLine($"Total low basin points excluding lowpoints: {basinPoints.Count}");
 
             
         }
+
 
         public static string[] ConvertInputToStringArray (string input)
         {
@@ -111,51 +125,38 @@ namespace AOCD9
             else return false;
         }
 
-        public static List<LowPoints> GetLowPointsBasinCenter(int[,] matrix, int x, int y)
+
+        public static List<Points> GetLowPointsBasinLeft(int[,] matrix, int x, int y)
         {
-            List<LowPoints> result = new List<LowPoints>();
-            var up = x - 1;
-            var down = x + 1;
+            List<Points> result = new List<Points>();
             var left = y - 1;
-            var right = y + 1;
-
-            if (x == 0) up = 1;
-            if (y == 0) left = 1;
-            if (y == matrix.GetLength(1) - 1) right = matrix.GetLength(1) - 2;
-            if (x == matrix.GetLength(0) - 1) down = matrix.GetLength(0) - 2;
-
-            if (matrix[x, y] < matrix[up, y] &&
-                matrix[x, y] < matrix[down, y] &&
-                matrix[x, y] < matrix[x, left] &&
-                matrix[x, y] < matrix[x, right]
-                )
-                result.Add(new LowPoints(matrix[x,y], x, y, matrix[x, y])); //classic lowpoints adding to result list
-
-            return result;
-        }
-
-        public static List<LowPoints> GetLowPointsBasinLeft(int[,] matrix, int x, int y)
-        {
-            List<LowPoints> result = new List<LowPoints>();
-            var left = y - 1;
+            int xInitialState = x;
+            int yInitialState = y;
+            if (y > 0)
+                left = y - 1;
+            else left = y;
             while (matrix[x, left] != 9 && left >= 0)
             {
                 if (matrix[x, left] > matrix[x, y] || matrix[x, left] != 9)
                 {
-                    //Console.WriteLine("Aggiungo un lowPoint Basin a sx");
-                    result.Add(new LowPoints (matrix[x,y], x, left, matrix[x, left]));
+                    result.Add(new Points(matrix[xInitialState, yInitialState], x, left, matrix[x, left]));
+                    result.AddRange(GetLowPointsBasinDown(matrix,x,left));
+                    result.AddRange(GetLowPointsBasinUp(matrix,x,left));
 
                 }
+
                 if (left == 0 || y == 0) break;
                     else { left--; y--; }
             }
             return result;
         }
 
-        public static List<LowPoints> GetLowPointsBasinRight(int[,] matrix, int x, int y)
+        public static List<Points> GetLowPointsBasinRight(int[,] matrix, int x, int y)
         {
-            List<LowPoints> result = new List<LowPoints>();
+            List<Points> result = new List<Points>();
             var right = y + 1;
+            int xInitialState = x;
+            int yInitialState = y;
             if (y < matrix.GetLength(1) - 1)
                 right = y + 1;
             else right = y;
@@ -164,7 +165,9 @@ namespace AOCD9
             {
                 if (matrix[x, right] > matrix[x, y])
                 {
-                    result.Add(new LowPoints(matrix[x,y], x, right, matrix[x, right]));
+                    result.Add(new Points(matrix[xInitialState, yInitialState], x, right, matrix[x, right]));
+                    result.AddRange(GetLowPointsBasinDown(matrix, x, right));
+                    result.AddRange(GetLowPointsBasinUp(matrix, x, right));
                 }
                 if (right == matrix.GetLength(1) - 1 || y == matrix.GetLength(1) - 1) break;
                 else { right++; y++; }
@@ -172,10 +175,12 @@ namespace AOCD9
             return result;
         }
 
-        public static List<LowPoints> GetLowPointsBasinUp(int[,] matrix, int x, int y)
+        public static List<Points> GetLowPointsBasinUp(int[,] matrix, int x, int y)
         {
-            List<LowPoints> result = new List<LowPoints>();
+            List<Points> result = new List<Points>();
             var up = x - 1;
+            int xInitialState = x;
+            int yInitialState = y;
             if (x > 0)
                 up = x - 1;
             else up = x;
@@ -183,7 +188,7 @@ namespace AOCD9
             {
                 if (matrix[up, y] > matrix[x, y])
                 {
-                    result.Add(new LowPoints(matrix[x,y], up, y, matrix[up, y])); //lowPointsBasin on the top
+                    result.Add(new Points(matrix[xInitialState, yInitialState], up, y, matrix[up, y])); //lowPointsBasin on the top
                 }
                 if (up == 0 || x == 0) break;
                 else { up--; x--; }
@@ -191,10 +196,12 @@ namespace AOCD9
             return result;
         }
 
-        public static List<LowPoints> GetLowPointsBasinDown(int[,] matrix, int x, int y)
+        public static List<Points> GetLowPointsBasinDown(int[,] matrix, int x, int y)
         {
-            List<LowPoints> result = new List<LowPoints>();
+            List<Points> result = new List<Points>();
             var down = x + 1;
+            int xInitialState = x;
+            int yInitialState = y;
             if (x < matrix.GetLength(0) - 1)
                 down = x + 1;
             else down = x;
@@ -202,8 +209,8 @@ namespace AOCD9
             {
                 if (matrix[down, y] > matrix[x, y])
                 {
-                    //Console.WriteLine("Aggiungo un lowPoint Basin down");
-                    result.Add(new LowPoints(matrix[x,y], down, y, matrix[down, y])); //lowPointsBasin on the bottom
+                    result.Add(new Points(matrix[xInitialState, yInitialState], down, y, matrix[down, y])); //lowPointsBasin on the bottom
+                    //result.AddRange(GetLowPointsBasinLeft(matrix, down, y));
                 }
                 if (down == matrix.GetLength(0) - 1 || x == matrix.GetLength(0) - 1) break;
                 else { down++; x++; }
@@ -211,148 +218,92 @@ namespace AOCD9
             return result;
         }
 
-        public static List<LowPoints> GetLowPointsBasin(int[,] matrix, List<LowPoints> lowPoints)
+        public static Dictionary<Points,int> CountBasin(int[,] matrix, List<Points> lowPoints)  //exluding lowpoints
         {
-            List<LowPoints> result = new List<LowPoints>();
-
-            foreach (var item in lowPoints)
+            List<Points> result = new List<Points>();
+            Dictionary<Points, int> resultCount = new Dictionary<Points, int>();
+            foreach (var point in lowPoints)
             {
-
-                //--------------- lowpoint basin on center -------------------------------
-                var x = item.CoordX;
-                var y = item.CoordY;
-                result.AddRange(GetLowPointsBasinCenter(matrix, x, y));
-
-                //var up = x - 1;
-                //var down = x + 1;
-                //var left = y - 1;
-                //var right = y + 1;
-
-                //if (x == 0) up = 1;
-                //if (y == 0) left = 1;
-                //if (y == matrix.GetLength(1) - 1) right = matrix.GetLength(1) - 2;
-                //if (x == matrix.GetLength(0) - 1) down = matrix.GetLength(0) - 2;
-
-                //if (matrix[x, y] < matrix[up, y] &&
-                //    matrix[x, y] < matrix[down, y] &&
-                //    matrix[x, y] < matrix[x, left] &&
-                //    matrix[x, y] < matrix[x, right]
-                //    )
-                //    result.Add(new LowPoints(item.LowPoint, x, y, matrix[x,y])); //classic lowpoints adding to result list
-
+                var x = point.CoordX;
+                var y = point.CoordY;
+                int resultcount = result.Count;
 
                 //--------------- lowpoint basin on the left ------------------------------
 
-                x = item.CoordX;
-                y = item.CoordY;
-                //while (matrix[x, left] != 9 && left >= 0)
-                //{
-                //    if (matrix[x, left] > matrix[x, y] || matrix[x, left] != 9)
-                //    {
-                //        //Console.WriteLine("Aggiungo un lowPoint Basin a sx");
-                //        result.Add(new LowPoints(item.LowPoint, x, left, matrix[x, left]));
-
-                //    }
-                //    if (left == 0 || y == 0) break;
-                //    else { left--; y--; }
-                //}
                 result.AddRange(GetLowPointsBasinLeft(matrix, x, y));
 
                 //----------------lowpoint basin on the right------------------------------
-                x = item.CoordX;
-                y = item.CoordY;
                 result.AddRange(GetLowPointsBasinRight(matrix, x, y));
-                //if (y < matrix.GetLength(1) - 1)
-                //    right = item.CoordY + 1;
-                //else right = y;
-                //while (matrix[x, right] != 9 && right < matrix.GetLength(1) - 1)
-                //{
-                //    if (matrix[x, right] > matrix[x, y])
-                //    {
-                //        result.Add(new LowPoints(item.LowPoint,x, right, matrix[x, right]));
-                //            //lowPointsBasin on the right
-                //    }
-                //    if (right == matrix.GetLength(1) - 1 || y == matrix.GetLength(1) - 1) break;
-                //    else { right++; y++; }
-                //}
 
                 //----------------lowpoint basin on up-------------------------------------
 
-
-                x = item.CoordX;
-                y = item.CoordY;
                 result.AddRange(GetLowPointsBasinUp(matrix, x, y));
-                                //if (x > 0)
-                //    up = item.CoordX - 1;
-                //else up = x;
-                //while (matrix[up, y] != 9 && up > 0)
-                //{
-                //    if (matrix[up, y] > matrix[x, y])
-                //    {
-                //        //Console.WriteLine("Aggiungo un lowPoint Basin up");
-                //        result.Add(new LowPoints(item.LowPoint, up, y, matrix[up, y])); //lowPointsBasin on the top
-                //    }
-                //    if (up == 0 || x == 0) break;
-                //    else { up--; x--; }
-                //}
 
-                //----------------lowpoint basin on down-------------------------------------
+                //----------------lowpoint basin on down-----------------------------------
 
-                x = item.CoordX;
-                y = item.CoordY;
                 result.AddRange(GetLowPointsBasinDown(matrix, x, y));
-                                //if (x < matrix.GetLength(0) - 1)
-                //    down = item.CoordX + 1;
-                //else down = x;
-                //while (matrix[down, y] != 9 && down <= matrix.GetLength(0) - 1)
-                //{
-                //    if (matrix[down, y] > matrix[x, y])
-                //    {
-                //        //Console.WriteLine("Aggiungo un lowPoint Basin down");
-                //        result.Add(new LowPoints(item.LowPoint, down, y, matrix[down, y])); //lowPointsBasin on the bottom
-                //    }
-                //    if (down == matrix.GetLength(0) - 1 || x == matrix.GetLength(0) - 1) break;
-                //    else { down++; x++; }
-                //}
+                int tempresult = result.Count - resultcount;
+                resultCount[point] = tempresult + 1;
+
+                //resultCount[point] = 
+            }
+            return resultCount;
+        }
+
+
+        public static List<Points> GetBasinPoints(int[,] matrix, List<Points> lowPoints)  //exluding lowpoints
+        {
+            List<Points> result = new List<Points>();
+            Dictionary<Points, int> resultCount = new Dictionary<Points, int>();
+            //List<Points> resultCount = new List<Points>();
+            foreach (var point in lowPoints)  
+            {
+                var x = point.CoordX;
+                var y = point.CoordY;
+                int resultcount = result.Count;
+
+                //--------------- lowpoint basin on the left ------------------------------
+
+                result.AddRange(GetLowPointsBasinLeft(matrix, x, y));
+
+                //----------------lowpoint basin on the right------------------------------
+                result.AddRange(GetLowPointsBasinRight(matrix, x, y));
+
+                //----------------lowpoint basin on up-------------------------------------
+
+                result.AddRange(GetLowPointsBasinUp(matrix, x, y));
+
+                //----------------lowpoint basin on down-----------------------------------
+
+                result.AddRange(GetLowPointsBasinDown(matrix, x, y));
+                int tempresult = result.Count-resultcount;
+                resultCount[point] = tempresult+1;
+
+                //resultCount[point] = 
             }
             return result;
         }
         
-
-        public static List<int> GetLowPoints (int[,] matrix)
+        public static List<Points> GetLowPoints (int[,] matrix)
         {
-            List<int> list = new List<int>();
-
+            List<Points> list = new List<Points>();
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    if (CheckLowPoint(matrix, row, col)) list.Add(matrix[row, col]);
+                    if (CheckLowPoint(matrix, row, col)) list.Add(new Points(matrix[row, col], row, col));
                 }
             }
             return list;
         }
 
-        public static List<LowPoints> GetLowPointsCoords(int[,] matrix)
-        {
-            List<LowPoints> list = new List<LowPoints>();
 
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    if (CheckLowPoint(matrix, row, col)) list.Add(new LowPoints { LowPoint = matrix[row, col], CoordX = row, CoordY = col });
-                }
-            }
-            return list;
-        }
-
-        public static int GetRiskLevel (List<int> list)
+        public static int GetRiskLevel (List<Points> list)
         {
             int result = 0;
             foreach (var item in list)
             {
-                result += item+1;
+                result += item.PointValue+1;
             }
             return result;
         }
